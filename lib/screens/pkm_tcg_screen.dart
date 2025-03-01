ddd9968bf78ff517dbb8cd806b18b987ddb6d433
jayden.tang <blueinnoclassroom@gmail.com>
2025-03-01 13:00:15 +0800

    l7 progress

--- lib/screens/pkm_tcg_screen.dart
+++ lib/screens/pkm_tcg_screen.dart
@@ -1,9 +1,9 @@
-import 'dart:ffi';
 import 'dart:math';
 
 import 'package:confetti/confetti.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:sigma_app/data/base1.dart';
 import 'package:sigma_app/data/pokemon-card.dart';
 import 'package:sigma_app/data/xy7.dart';
@@ -22,6 +22,17 @@ class _PkmTcgScreenState extends State<PkmTcgScreen> {
 
   final _allCards = <PokemonCard>[];
   final _drawResults = <PokemonCard>[];
+
+  final rarities = [
+    ('Common', 60, Colors.black),
+    ('Uncommon', 30, Colors.black),
+    ('Rare', 9, Colors.green[500]),
+    ('Rare Holo', 0.7, Colors.yellow[800]),
+    ('Rare Ultra', 0.29889, Colors.orange[900]),
+    ('Rare Holo EX', 0.001, Colors.red[900]),
+    ('Rare BREAK', 0.0001, Colors.blue[900]),
+    ('Rare Secret', 0.00001, Colors.purple[600]),
+  ];
 
   @override
   void initState() {
@@ -43,40 +54,7 @@ class _PkmTcgScreenState extends State<PkmTcgScreen> {
         actions: [
           IconButton(
             icon: Icon(Icons.refresh),
-            onPressed: () {
-              final rarities = [
-                ('Common', 60),
-                ('Uncommon', 30),
-                ('Rare', 9),
-                ('Rare Holo EX', 1),
-              ];
-              final random = Random().nextDouble() * 100;
-              var cumulative = 0;
-              var index = 0;
-
-              for (final rarity in rarities) {
-                cumulative += rarity.$2;
-                if (random <= cumulative) {
-                  index = rarities.indexOf(rarity);
-                  break;
-                }
-              }
-              final rar = rarities[index];
-              final pool = _allCards.where((e) => e.rarity == rar.$1).toList();
-              _drawResults.clear();
-              pool.shuffle();
-              _drawResults.addAll(pool.take(8));
-
-              setState(() {}); // refresh the page
-              _drawResults.clear();
-              _allCards.shuffle();
-              _drawResults.addAll(_allCards.take(8));
-
-              final hasRare =
-                  _drawResults.any((e) => e.rarity?.contains('Rare') ?? false);
-              if (hasRare) controller.play();
-              setState(() {});
-            },
+            onPressed: () => _drawCard(),
           ),
         ],
       ),
@@ -86,7 +64,7 @@ class _PkmTcgScreenState extends State<PkmTcgScreen> {
             itemCount: _drawResults.length,
             gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
               maxCrossAxisExtent: 245,
-              childAspectRatio: 245 / 342,
+              childAspectRatio: 245 / (342 + 45),
             ),
             itemBuilder: (context, index) {
               // final raw = _drawResults[index];
@@ -96,10 +74,27 @@ class _PkmTcgScreenState extends State<PkmTcgScreen> {
 
               if (image == null) return Placeholder();
 
-              return Card(child: Image.network(image))
-                  .animate()
-                  .fadeIn(delay: 0.2.seconds)
-                  .flipH();
+              final result = rarities.firstWhere(
+                (e) => e.$1 == pkmCard.rarity,
+                orElse: () => rarities.last,
+              );
+
+              return Column(
+                children: [
+                  Text(
+                    '${result.$1} (${result.$2}%)',
+                    style: GoogleFonts.getFont(
+                      'Press Start 2P',
+                      fontSize: 15,
+                      color: result.$3,
+                    ),
+                  ),
+                  Card(child: Image.network(image))
+                      .animate()
+                      .fadeIn(delay: 0.2.seconds)
+                      .flipH(),
+                ],
+              );
             },
           ),
           Positioned(
@@ -135,4 +130,34 @@ class _PkmTcgScreenState extends State<PkmTcgScreen> {
       ),
     );
   }
+
+  void _drawCard() {
+    _drawResults.clear();
+
+    for (var i = 0; i < 9; i++) {
+      final random = Random().nextDouble() * 100;
+      var cumulative = 0.0;
+      var index = 0;
+
+      for (final rarity in rarities) {
+        cumulative += rarity.$2;
+        if (random <= cumulative) {
+          index = rarities.indexOf(rarity);
+          break;
+        }
+      }
+      final rar = rarities[index];
+      final pool = _allCards.where((e) => e.rarity == rar.$1).toList();
+      pool.shuffle();
+      _drawResults.add(pool.first);
+    }
+
+    final hasRare =
+        _drawResults.any((e) => e.rarity?.contains('Rare Holo EX') ?? false);
+    if (hasRare) controller.play();
+
+    setState(() {}); // refresh the page
+  }
 }
+
+// chances of geting rare holo ex is 0.001 confetti will show if rare holo ex
