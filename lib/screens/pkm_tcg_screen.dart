a6e143fc139ac6a66ae290297ba570ea5950c5de
jayden.tang <blueinnoclassroom@gmail.com>
2025-03-22 12:33:24 +0800

    erauhrguhealirgfsgadagsdgsd

--- lib/screens/pkm_tcg_screen.dart
+++ lib/screens/pkm_tcg_screen.dart
@@ -1,54 +1,130 @@
+import 'dart:convert';
 import 'dart:math';
 
 import 'package:confetti/confetti.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:sigma_app/data/base1.dart';
-import 'package:sigma_app/data/pokemon-card.dart';
-import 'package:sigma_app/data/xy7.dart';
-import 'package:sigma_app/widgets/route_drawer.dart';
+
+import '../data/pokemon-card.dart';
 
 class PkmTcgScreen extends StatefulWidget {
-  const PkmTcgScreen({super.key});
+  const PkmTcgScreen({
+    super.key,
+    this.packPaths,
+    this.onDraw,
+  });
+
+  final List<String>? packPaths;
+  final void Function(double totalValue)? onDraw;
 
   @override
   State<PkmTcgScreen> createState() => _PkmTcgScreenState();
 }
 
 class _PkmTcgScreenState extends State<PkmTcgScreen> {
-  final controller = ConfettiController(duration: Duration(seconds: 1));
+  final controller = ConfettiController(duration: Duration(seconds: 2));
   // ..play();
 
   final _allCards = <PokemonCard>[];
   final _drawResults = <PokemonCard>[];
 
-  final rarities = [
-    ('Common', 60, Colors.black),
-    ('Uncommon', 30, Colors.black),
-    ('Rare', 9, Colors.green[500]),
-    ('Rare Holo', 0.7, Colors.yellow[800]),
-    ('Rare Ultra', 0.29889, Colors.orange[900]),
-    ('Rare Holo EX', 0.001, Colors.red[900]),
-    ('Rare BREAK', 0.0001, Colors.blue[900]),
-    ('Rare Secret', 0.00001, Colors.purple[600]),
-  ];
+  final rareProbs = {
+    'Amazing Rare': 1.02,
+    'Shiny Ultra Rare': 0.02,
+    'Rare Ultra': 0.03,
+    'Rare ACE': 0.02,
+    'Rare Rainbow': 0.02,
+    'Rare Shiny GX': 0.02,
+    'Rare Holo VSTAR': 0.03,
+    'Rare Shiny': 0.02,
+    'Double Rare': 0.04,
+    'ACE SPEC Rare': 0.02,
+    'Promo': 0.03,
+    'Classic Collection': 0.03,
+    'Rare Secret': 0.02,
+    'Rare Holo Star': 0.02,
+    'Rare BREAK': 0.02,
+    'Rare Prism Star': 0.02,
+    'Ultra Rare': 0.04,
+    'Rare Holo LV.X': 0.02,
+    'Hyper Rare': 0.1,
+    'Rare Prime': 0.02,
+    'Rare Shining': 0.02,
+    'Trainer Gallery Rare Holo': 0.02,
+    'Radiant Rare': 0.2,
+    'Rare': 1.0,
+    'Rare Holo VMAX': 0.02,
+    'Rare Holo EX': 0.02,
+    'Special Illustration Rare': 0.01,
+    'Illustration Rare': 0.03,
+    'Shiny Rare': 0.02,
+    'Rare Holo': 0.04,
+    'Rare Holo GX': 0.02,
+    'Rare Holo V': 0.02,
+    'LEGEND': 50,
+  };
+
+  final sellingPriceList = {
+    'Amazing Rare': 100.00,
+    'Shiny Ultra Rare': 80.00,
+    'Rare Ultra': 60.00,
+    'Rare ACE': 70.00,
+    'Rare Rainbow': 90.00,
+    'Rare Shiny GX': 80.00,
+    'Rare Holo VSTAR': 6.50,
+    'Rare Shiny': 50.00,
+    'Double Rare': 40.00,
+    'ACE SPEC Rare': 7.50,
+    'Promo': 5.50,
+    'Classic Collection': 60.00,
+    'Rare Secret': 80.00,
+    'Rare Holo Star': 5.50,
+    'Rare BREAK': 50.00,
+    'Rare Prism Star': 50.00,
+    'Ultra Rare': 70.00,
+    'Rare Holo LV.X': 60.00,
+    'Hyper Rare': 150.00,
+    'Rare Prime': 5.50,
+    'Rare Shining': 50.00,
+    'Trainer Gallery Rare Holo': 60.00,
+    'Radiant Rare': 50.00,
+    'Rare': 1.00,
+    'Rare Holo VMAX': 6.50,
+    'Rare Holo EX': 50.00,
+    'Special Illustration Rare': 120.00,
+    'Illustration Rare': 70.00,
+    'Shiny Rare': 50.00,
+    'Rare Holo': 4.50,
+    'Rare Holo GX': 5.50,
+    'Rare Holo V': 50.00,
+    'LEGEND': 2000.00,
+  };
 
   @override
   void initState() {
     super.initState();
 
-    _allCards.addAll(base1.map((e) => PokemonCard.fromJson(e)));
-    _allCards.addAll(xy7.map((e) => PokemonCard.fromJson(e)));
-    _allCards.shuffle();
+    final paths = widget.packPaths ?? ['base1'];
+    final futures = paths.map((e) => rootBundle.loadString('tcg_data/$e.json'));
 
-    _drawCard();
+    Future.wait(futures).then((files) {
+      final allCards = files.map((fileStr) {
+        final cards = jsonDecode(fileStr) as List;
+        return cards.map((e) => PokemonCard.fromJson(e));
+      }).expand((e) => e);
+
+      _allCards.addAll(allCards);
+      _allCards.shuffle();
+
+      _drawCard();
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // drawer: RouteDrawer(),
       // appBar: AppBar(
       //   title: Text('PKM TCG'),
       //   actions: [
@@ -58,41 +134,51 @@ class _PkmTcgScreenState extends State<PkmTcgScreen> {
       //     ),
       //   ],
       // ),
+      // drawer: RouteDrawer(),
       body: Stack(
         children: [
           GridView.builder(
             itemCount: _drawResults.length,
             gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
               maxCrossAxisExtent: 245,
-              childAspectRatio: 245 / (342 + 45),
+              childAspectRatio: 245 / (342 + 20),
             ),
             itemBuilder: (context, index) {
-              // final raw = _drawResults[index];
-              // final pkmCard = PokemonCard.fromJson(raw);
               final pkmCard = _drawResults[index];
               final image = pkmCard.images?.small;
 
               if (image == null) return Placeholder();
 
-              final result = rarities.firstWhere(
-                (e) => e.$1 == pkmCard.rarity,
-                orElse: () => rarities.last,
-              );
+              final rarity = pkmCard.rarity;
+              // final color = rareColors[rarity];
+              final prob = rareProbs[rarity];
 
               return Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text(
-                    '${result.$1} (${result.$2}%)',
-                    style: GoogleFonts.getFont(
-                      'Press Start 2P',
-                      fontSize: 15,
-                      color: result.$3,
-                    ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        rarity ?? 'Unknown',
+                        style: GoogleFonts.getFont(
+                          'Press Start 2P',
+                          fontSize: 8,
+                          // color: color,
+                        ),
+                      ),
+                      if (prob != null)
+                        Text(
+                          '($prob%)',
+                          style: GoogleFonts.getFont(
+                            'Press Start 2P',
+                            fontSize: 8,
+                            // color: color,
+                          ),
+                        ),
+                    ],
                   ),
-                  Card(child: Image.network(image))
-                      .animate()
-                      .fadeIn(delay: 0.2.seconds)
-                      .flipH(),
+                  Card(child: Image.network(image)).animate().fadeIn(),
                 ],
               );
             },
@@ -103,8 +189,9 @@ class _PkmTcgScreenState extends State<PkmTcgScreen> {
             child: ConfettiWidget(
               confettiController: controller,
               shouldLoop: false,
-              blastDirection: 0.7,
-              emissionFrequency: 1,
+              blastDirection: 0.785, // 45º
+              emissionFrequency: 0.1,
+              numberOfParticles: 50,
             ),
           ),
           Positioned(
@@ -113,8 +200,9 @@ class _PkmTcgScreenState extends State<PkmTcgScreen> {
             child: ConfettiWidget(
               confettiController: controller,
               shouldLoop: false,
-              blastDirection: 1.5 / 2 * pi,
-              emissionFrequency: 1,
+              blastDirection: 2.356, // 135º
+              emissionFrequency: 0.1,
+              numberOfParticles: 50,
             ),
           ),
           Align(
@@ -123,7 +211,8 @@ class _PkmTcgScreenState extends State<PkmTcgScreen> {
               confettiController: controller,
               shouldLoop: false,
               blastDirection: pi / 2,
-              emissionFrequency: 1,
+              emissionFrequency: 0.1,
+              numberOfParticles: 50,
             ),
           ),
         ],
@@ -134,30 +223,51 @@ class _PkmTcgScreenState extends State<PkmTcgScreen> {
   void _drawCard() {
     _drawResults.clear();
 
-    for (var i = 0; i < 9; i++) {
+    final availableRares = _allCards
+        .map((e) => e.rarity)
+        .toSet()
+        .where((e) => e != null && e != 'Common' && e != 'Uncommon');
+
+    final rarities = <String, num>{};
+    var remaining = 100.0;
+    for (final r in availableRares) {
+      rarities[r!] = rareProbs[r]!;
+      remaining -= rareProbs[r]!;
+    }
+    rarities['Uncommon'] = remaining * 0.20;
+    rarities['Common'] = remaining * 0.80;
+
+    for (var i = 0; i < 15; i++) {
       final random = Random().nextDouble() * 100;
       var cumulative = 0.0;
-      var index = 0;
+      var rarity = '';
 
-      for (final rarity in rarities) {
-        cumulative += rarity.$2;
+      for (final entry in rarities.entries) {
+        final prob = entry.value;
+
+        cumulative += prob;
         if (random <= cumulative) {
-          index = rarities.indexOf(rarity);
+          rarity = entry.key;
           break;
         }
       }
-      final rar = rarities[index];
-      final pool = _allCards.where((e) => e.rarity == rar.$1).toList();
-      pool.shuffle();
-      _drawResults.add(pool.first);
+
+      final filtered = _allCards.where((e) => e.rarity == rarity).toList();
+
+      filtered.shuffle();
+      _drawResults.addAll(filtered.take(1));
     }
 
-    final hasRare =
-        _drawResults.any((e) => e.rarity?.contains('Rare Holo EX') ?? false);
+    final hasRare = _drawResults.any((e) =>
+        e.rarity != null && e.rarity != 'Common' && e.rarity != 'Uncommon');
     if (hasRare) controller.play();
+
+    var totalValue = 0.0;
+    for (final card in _drawResults) {
+      totalValue += sellingPriceList[card.rarity] ?? 0;
+    }
+    widget.onDraw?.call(totalValue);
 
     setState(() {}); // refresh the page
   }
 }
-
-// chances of geting rare holo ex is 0.001 confetti will show if rare holo ex
--- lib/survival_screen.dart
+++ lib/survival_screen.dart
@@ -17,67 +17,194 @@ class _SurvivalScreenState extends State<SurvivalScreen> {
       (
         'Base',
         70,
-        'https://cc-client-assets.nyc3.cdn.digitaloceanspaces.com/photo/collectorscache/file/0b3349f0555211e89b154f403011cb11/base%20set%20charizard.jpg'
+        'https://cc-client-assets.nyc3.cdn.digitaloceanspaces.com/photo/collectorscache/file/0b3349f0555211e89b154f403011cb11/base%20set%20charizard.jpg',
+        [
+          'base1',
+          'base2',
+          'base3',
+          'base4',
+          'base5',
+          'base6',
+          'basep',
+        ]
       ),
       (
         'Black & White',
         50,
-        'https://m.media-amazon.com/images/I/71LZgYZTr-L.jpg'
+        'https://m.media-amazon.com/images/I/71LZgYZTr-L.jpg',
+        [
+          'bw1',
+          'bw2',
+          'bw3',
+          'bw4',
+          'bw5',
+          'bw6',
+          'bw7',
+          'bw8',
+          'bw9',
+          'bw10',
+          'bw11',
+          'bwp',
+        ]
       ),
       (
         'Colosseum',
         50,
-        'https://archives.bulbagarden.net/media/upload/thumb/2/25/Colosseum_EN_boxart.png/250px-Colosseum_EN_boxart.png'
+        'https://lens.usercontent.google.com/image?vsrid=COadxd7ru630AhACGAEiJGE2MjY4ODJmLTFjM2UtNDE2NS1hOTM4LTRjZjg0M2RkMjE4Ng&gsessionid=kASkypyhmv4XBy4MFcQPh3JaVATp3Jf4mwo2UYiw1csICh_w5dFz6Q',
+        ['dp1', 'dp2', 'dp3', 'dp4', 'dp5', 'dp6', 'dp7', 'dpp']
       ),
       (
         'Diamond & Pearl',
         40,
-        'https://storage.googleapis.com/images.pricecharting.com/d7bc1dee360a08f332423ba648f8eecb27d51f46bba3a02863158b0e6e29f1a8/1600.jpg'
+        'https://storage.googleapis.com/images.pricecharting.com/d7bc1dee360a08f332423ba648f8eecb27d51f46bba3a02863158b0e6e29f1a8/1600.jpg',
+        ['col1', 'cel25', 'cel25c']
       ),
       (
         'E-Card',
         10,
-        'https://loosepacks.com/cdn/shop/files/Untitled-1_75_590x590.png?v=1738787657'
+        'https://loosepacks.com/cdn/shop/files/Untitled-1_75_590x590.png?v=1738787657',
+        ['ecard1', 'ecard2', 'ecard3']
       ),
       (
         'Ex',
         40,
-        'https://tcgplayer-cdn.tcgplayer.com/product/565245_in_1000x1000.jpg'
+        'https://tcgplayer-cdn.tcgplayer.com/product/565245_in_1000x1000.jpg',
+        [
+          'ex1',
+          'ex2',
+          'ex3',
+          'ex4',
+          'ex5',
+          'ex6',
+          'ex7',
+          'ex8',
+          'ex9',
+          'ex10',
+          'ex11',
+          'ex12',
+          'ex13',
+          'ex14',
+          'ex15',
+          'ex16',
+        ]
       ),
       (
         'Gym',
         20,
-        'https://tools.toywiz.com/_images/_webp/_products/lg/ampokgymchal1st.webp'
+        'https://tools.toywiz.com/_images/_webp/_products/lg/ampokgymchal1st.webp',
+        [
+          'gym1',
+          'gym2',
+        ]
       ),
       (
         'HeartGold and SoulSilver',
         30,
-        'https://i.ebayimg.com/images/g/WFsAAOSw7mhebZHB/s-l1200.jpg'
+        'https://i.ebayimg.com/images/g/WFsAAOSw7mhebZHB/s-l1200.jpg',
+        [
+          'hgss1',
+          'hgss2',
+          'hgss3',
+          'hgss4',
+          'hsp',
+        ]
       ),
       (
         'Neo Genesis',
         50,
-        'https://tcgplayer-cdn.tcgplayer.com/product/138142_in_1000x1000.jpg'
+        'https://tcgplayer-cdn.tcgplayer.com/product/138142_in_1000x1000.jpg',
+        [
+          'neo1',
+          'neo2',
+          'neo3',
+          'neo4',
+          'neop',
+        ]
       ),
       (
         'Platinum',
         50,
-        'https://m.media-amazon.com/images/I/717c7HvT9OL._AC_UF1000,1000_QL80_.jpg'
+        'https://m.media-amazon.com/images/I/717c7HvT9OL._AC_UF1000,1000_QL80_.jpg',
+        [
+          'pl1',
+          'pl2',
+          'pl3',
+          'pl4',
+        ]
       ),
       (
         'Sun & Moon',
         30,
-        'https://i5.walmartimages.com/seo/Pokemon-Sun-Moon-SM1-Booster-Pack-Lunala_324ef300-9c40-49a3-9b3b-f4acb8e1dfcc.d0b3d15db3b2ebfd777f8ca55a319f46.png'
+        'https://i5.walmartimages.com/seo/Pokemon-Sun-Moon-SM1-Booster-Pack-Lunala_324ef300-9c40-49a3-9b3b-f4acb8e1dfcc.d0b3d15db3b2ebfd777f8ca55a319f46.png',
+        [
+          'sm1',
+          'sm2',
+          'sm3',
+          'sm4',
+          'sm5',
+          'sm6',
+          'sm7',
+          'sm8',
+          'sm9',
+          'sm10',
+          'sm11',
+          'sm12',
+          'sm35',
+          'sm75',
+          'sm115',
+          'sma',
+          'smp'
+        ]
       ),
       (
         'Sword & Shield',
         20,
-        'https://m.media-amazon.com/images/I/61tQ+TAjYoL._AC_UF894,1000_QL80_.jpg'
+        'https://m.media-amazon.com/images/I/61tQ+TAjYoL._AC_UF894,1000_QL80_.jpg',
+        [
+          'swsh1',
+          'swsh2',
+          'swsh3',
+          'swsh4',
+          'swsh5',
+          'swsh6',
+          'swsh7',
+          'swsh8',
+          'swsh9',
+          'swsh9tg',
+          'swsh10',
+          'swsh10tg',
+          'swsh11',
+          'swsh11tg',
+          'swsh12',
+          'swsh12pt5',
+          'swsh12pt5gg',
+          'swsh12tg',
+          'swsh35',
+          'swsh45',
+          'swsh45sv',
+          'swshp'
+        ]
       ),
       (
         'XY',
         50,
-        'https://m.media-amazon.com/images/I/71Ax+NvhZCL._AC_UF894,1000_QL80_.jpg'
+        'https://m.media-amazon.com/images/I/71Ax+NvhZCL._AC_UF894,1000_QL80_.jpg',
+        [
+          'xy0',
+          'xy1',
+          'xy2',
+          'xy3',
+          'xy4',
+          'xy5',
+          'xy6',
+          'xy7',
+          'xy8',
+          'xy9',
+          'xy10',
+          'xy11',
+          'xy12',
+          'xyp'
+        ]
       ),
     ];
     final SellingPriceList = {
@@ -137,7 +264,12 @@ class _SurvivalScreenState extends State<SurvivalScreen> {
 
                       showDialog(
                         context: context,
-                        builder: (context) => PkmTcgScreen(),
+                        builder: (context) => PkmTcgScreen(
+                            packPaths: pack.$4,
+                            onDraw: (totalValue) {
+                              wallet += totalValue;
+                              setState(() {});
+                            }),
                       );
                     },
                     child: Image.network(imgUrl),
